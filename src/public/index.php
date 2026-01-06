27113b16659293d718e6cf26986e882a5eb19e1d
owner <owner@localhost>
2026-01-06 12:19:02 +0900

    tailwind追加

---
+++ src/public/index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+  <link href="/build/css/app.css" rel="stylesheet">
+</head>
+<body class="bg-gray-100 p-8">
+  <h1 class="text-3xl font-bold text-blue-600">
+    Tailwind OK
+  </h1>
+</body>
+</html>
