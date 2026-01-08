f9482d19cfc30919120359000dcdb0a4edecd504
admin <admin@locahost.com>
2026-01-08 12:37:49 +0900

    コードの整備

--- src/app/Services/Frontend/Vite.php
+++ src/app/Services/Frontend/Vite.php
@@ -16,16 +16,7 @@ class Vite
         $val = '';
 
         if (self::isDev()) {
-            $val = '
-                <script type="module">
-                    import RefreshRuntime from "' . self::devUrl() . '/@react-refresh"
-                    RefreshRuntime.injectIntoGlobalHook(window)
-                    window.$RefreshReg$ = () => {}
-                    window.$RefreshSig$ = () => (type) => type
-                    window.__vite_plugin_react_preamble_installed__ = true
-                </script>
-                <script type="module" src="' . self::devUrl() . '/@vite/client"></script>
-            ';
+            $val = '<script type="module" src="' . self::devUrl() . '/@vite/client"></script>' . self::reactRefresh();
         }
 
         return $val;
@@ -37,13 +28,33 @@ class Vite
         if (self::isDev()) {
             return self::devUrl() . '/' . $entry;
         } else {
-            if (self::$manifest === null) {
-                $path = APP_ROOT . '/public/assets/.vite/manifest.json';
-                self::$manifest = json_decode(file_get_contents($path), true);
-            }
+            self::initManifest();
 
             return '/assets/' . self::$manifest[$entry]['file'];
         }
+    }
+
+    /** Manifestを取得 */
+    private static function initManifest()
+    {
+        if (self::$manifest === null) {
+            $path = APP_ROOT . '/public/assets/.vite/manifest.json';
+            self::$manifest = json_decode(file_get_contents($path), true);
+        }
+    }
+
+    /** ViteのReactプラグインを使うときに必要なコード */
+    private static function reactRefresh(): string
+    {
+        return '
+            <script type="module">
+                import RefreshRuntime from "' . self::devUrl() . '/@react-refresh"
+                RefreshRuntime.injectIntoGlobalHook(window)
+                window.$RefreshReg$ = () => {}
+                window.$RefreshSig$ = () => (type) => type
+                window.__vite_plugin_react_preamble_installed__ = true
+            </script>
+        ';
     }
 
     /** Viteが開発環境か返す */
