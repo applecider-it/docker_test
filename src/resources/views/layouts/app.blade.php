df1c9d6523008155f78409f7322c2caeab9bbe76
owner <owner@localhost>
2026-02-24 12:07:21 +0900

    アイコン追加

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -7,6 +7,8 @@ use App\Services\Javascript\ViteService;
 <head>
     <meta charset="UTF-8">
 
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg">
+
     {!! ViteService::init() !!}
     <link rel="stylesheet" href="{{ ViteService::asset('resources/css/app.css') }}">
     <script type="module" src="{{ ViteService::asset('resources/js/entrypoints/app.ts') }}"></script>
