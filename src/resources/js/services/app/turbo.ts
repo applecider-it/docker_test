aab4e6f08ddb0fe3769a117f393a0b7666d9ac8b
owner <owner@localhost>
2026-03-01 21:20:20 +0900

    turboにアンロード時の処理

--- src/resources/js/services/app/turbo.ts
+++ src/resources/js/services/app/turbo.ts
@@ -7,8 +7,12 @@
  * ・ページ遷移後に読み込まれる
  * ・読み込まれた後に、再度、ページ遷移してくるとき
  */
-export function setupTurboLoad(setup: Function, flagId: string) {
-  const func = () => {
+export function setupTurboLoad(
+  flagId: string,
+  load: Function,
+  unload: Function,
+) {
+  const execLoad = () => {
     const flagObj = document.getElementById(flagId);
 
     // フラグ用オブジェクトがないとき（対象のページじゃないとき）
@@ -21,13 +25,26 @@ export function setupTurboLoad(setup: Function, flagId: string) {
     // フラグ用オブジェクトの中身が更新済み（既に初期化されているとき）
     if (isInit) return;
 
-    setup();
+    load();
+  };
+
+  const execUnload = () => {
+    const flagObj = document.getElementById(flagId);
+
+    // フラグ用オブジェクトがないとき（対象のページじゃないとき）
+    if (!flagObj) return;
+
+    unload();
   };
 
   document.addEventListener("turbo:load", () => {
-    func();
+    execLoad();
+  });
+
+  document.addEventListener("turbo:before-cache", () => {
+    execUnload();
   });
 
   // 「・読み込まれるページでリロード」このパターンで動作させるために必要な措置
-  func();
+  execLoad();
 }
--- src/resources/js/services/development/setup-javascript-test.tsx
+++ src/resources/js/services/development/setup-javascript-test.tsx
@@ -8,12 +8,24 @@ import { setupTurboLoad } from "@/services/app/turbo";
 
 const flagId = "app-page-development-javascript-test";
 
+const test = () => {
+  console.log("test");
+};
+
 const setup = () => {
   console.log("init");
 
   createApp(AppVue).mount("#vue");
 
   ReactDOM.createRoot(document.getElementById("react")).render(<AppReact />);
+
+  document.addEventListener("click", test);
 };
 
-setupTurboLoad(setup, flagId);
+const clear = () => {
+  console.log("unload");
+
+  document.removeEventListener("click", test);
+};
+
+setupTurboLoad(flagId, setup, clear);
